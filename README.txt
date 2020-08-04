09943af14b87831df0d912ff385384d6187da9d1
devkosal <devkosal@gmail.com>
2020-08-03 21:47:03 -0500

    added source code

---
+++ README.txt
@@ -0,0 +1,6 @@
+Example usage:
+
+"""
+sys.path.append("/Users/devsharma/Projects/base_code")
+from sharma import tree_view
+"""
---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name="base_code",
+    author="Dev Sharma",
+    author_email="devkosal@gmail.com",
+    url="https://github.com/devkosal/base_code",
+    packages=["base_code"])
