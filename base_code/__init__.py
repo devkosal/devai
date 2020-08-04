a7f92aeed10674648eebf89a50dc9a1fddfd36d8
devkosal <devkosal@gmail.com>
2020-08-03 22:02:45 -0500

    created package

--- base_code/__init__.py
+++ base_code/__init__.py
@@ -1,13 +1,13 @@
-from .utils import *
-from .utils_ml import *
-from .basics import *
-from .callbacks import *
-from .optimizers import *
-from .hooks import *
-from .dataloaders import *
-from .metrics import *
-from .loss_funcs import *
-from .activation_funcs import *
-from .text import *
-from .model import *
+# from .utils import *
+# from .utils_ml import *
+# from .basics import *
+# from .callbacks import *
+# from .optimizers import *
+# from .hooks import *
+# from .dataloaders import *
+# from .metrics import *
+# from .loss_funcs import *
+# from .activation_funcs import *
+# from .text import *
+# from .model import *
 
--- setup.py
+++ setup.py
@@ -5,3 +5,25 @@ setup(name="base_code",
     author_email="devkosal@gmail.com",
     url="https://github.com/devkosal/base_code",
     packages=["base_code"])
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="base_code", # Replace with your own username
+    version="0.0.1",
+    author="Dev Sharma",
+    author_email="devkosal@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/devkosal/base_code",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
